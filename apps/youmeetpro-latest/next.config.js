4b370c420f915f9f1e41c42b037397f5cafc873b
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-08-01 23:53:03 +0200

    set preview script

--- apps/youmeetpro-latest/next.config.js
+++ apps/youmeetpro-latest/next.config.js
@@ -1,7 +1,6 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
