418fcf61221838068bc03ecd2867d11a50bc7061
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-10-21 15:49:45 +0200

    modified log in route

--- apps/youmeet-latest/app/api/auth/francetravail/route.ts
+++ apps/youmeet-latest/app/api/auth/francetravail/route.ts
@@ -1,6 +1,18 @@
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  console.log(req.url, "req.url");
+  console.log(req.nextUrl.toString(), "req.url");
+
+  const code = req.nextUrl.searchParams.get("code");
+  const uri =
+    "https://authentification-candidat.francetravail.fr/connexion/oauth2/access_token/individu";
+  const response = await fetch(uri, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      code,
+    }),
+  });
   return Response.json("Hello, world!");
 }
