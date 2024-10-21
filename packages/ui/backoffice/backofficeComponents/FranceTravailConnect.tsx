f55cd7eea98c4b431bc6b33227795fdbc0d25052
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-10-21 17:36:20 +0200

    set button in backoffice to request access_token

--- packages/ui/backoffice/backofficeComponents/FranceTravailConnect.tsx
+++ packages/ui/backoffice/backofficeComponents/FranceTravailConnect.tsx
@@ -16,26 +16,45 @@ export default function FranceTravailConnect() {
     <ft-connect
       data-mode="light"
       onClick={async () => {
-        const uri =
-          "https://authentification-candidat.francetravail.fr/connexion/oauth2/authorize";
+        // const uri =
+        //   "https://authentification-candidat.francetravail.fr/connexion/oauth2/authorize";
         const realm = "individu";
         const client_id = `${process.env.FRANCE_TRAVAIL_CLIENT_ID}`;
+        const client_secret = `${process.env.FRANCE_TRAVAIL_CLIENT_SECRET}`;
         const scope = "api_stats-perspectives-retour-emploiv1 retouremploi";
         const redirect_uri = "https://www.youmeet.info/api/auth/francetravail";
 
-        const params = new URLSearchParams({
-          realm,
-          response_type: "code",
-          client_id,
-          scope,
-          redirect_uri,
-          state: "ABC",
-          nonce: "DEF",
-        });
-        console.log(params.toString(), "params");
+        // const params = new URLSearchParams({
+        //   realm,
+        //   response_type: "code",
+        //   client_id,
+        //   scope,
+        //   redirect_uri,
+        //   state: "ABC",
+        //   nonce: "DEF",
+        // });
+        // console.log(params.toString(), "params");
 
-        const endpoint = uri + "?" + params.toString();
-        await fetch(endpoint, { method: "GET" });
+        // const endpoint = uri + "?" + params.toString();
+        // await fetch(endpoint, { method: "GET" });
+
+        const uri =
+          "https://entreprise.francetravail.fr/connexion/oauth2/access_token";
+        const response = await fetch(uri, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: JSON.stringify({
+            grant_type: "client_credentials",
+            client_id,
+            client_secret,
+            scope: "api_offresdemploiv2 o2dsoffre",
+          }),
+          mode: "no-cors",
+        });
+        const data = await response.json();
+        console.log(data, "data");
       }}
     ></ft-connect>
   );
