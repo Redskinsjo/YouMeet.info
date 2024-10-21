86c6fe28978b4beff3910739a69583b19ae12206
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-10-21 15:51:52 +0200

    added log for access-token in route

--- apps/youmeet-latest/app/api/auth/francetravail/route.ts
+++ apps/youmeet-latest/app/api/auth/francetravail/route.ts
@@ -6,13 +6,21 @@ export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get("code");
   const uri =
     "https://authentification-candidat.francetravail.fr/connexion/oauth2/access_token/individu";
+  const client_id = `${process.env.FRANCE_TRAVAIL_CLIENT_ID}`;
+  const client_secret = `${process.env.FRANCE_TRAVAIL_CLIENT_SECRET}`;
+  const redirect_uri = "https://www.youmeet.info/api/auth/francetravail";
   const response = await fetch(uri, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: JSON.stringify({
       grant_type: "authorization_code",
       code,
+      client_id,
+      client_secret,
+      redirect_uri,
     }),
   });
+  const data = await response.json();
+  console.log(data, "data");
   return Response.json("Hello, world!");
 }
