eb8c4eefb63983d08894cc42196615ab992aeabe
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-10-24 17:08:09 +0200

    updated turbo and removed comments in ft-connect

--- packages/ui/backoffice/backofficeComponents/FranceTravailConnect.tsx
+++ packages/ui/backoffice/backofficeComponents/FranceTravailConnect.tsx
@@ -18,28 +18,6 @@ export default function FranceTravailConnect() {
     <ft-connect
       data-mode="light"
       onClick={async () => {
-        // const uri =
-        //   "https://authentification-candidat.francetravail.fr/connexion/oauth2/authorize";
-        // const realm = "/individu";
-        // const client_id = `${process.env.FRANCE_TRAVAIL_CLIENT_ID}`;
-        // const client_secret = `${process.env.FRANCE_TRAVAIL_CLIENT_SECRET}`;
-        // const scope = "api_stats-perspectives-retour-emploiv1 retouremploi";
-        // const redirect_uri = "https://www.youmeet.info/api/auth/francetravail";
-
-        // const params = new URLSearchParams({
-        //   realm,
-        //   response_type: "code",
-        //   client_id,
-        //   scope,
-        //   redirect_uri,
-        //   state: "ABC",
-        //   nonce: "DEF",
-        // });
-        // console.log(params.toString(), "params");
-
-        // const endpoint = uri + "?" + params.toString();
-        // await fetch(endpoint, { method: "GET" });
-
         const response = await fetch(`${uri}/api/access_francetravail`, {
           method: "GET",
         });
