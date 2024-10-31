e7295168bc666122be2d66a5cc8524115f804c5d
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-10-31 22:23:29 +0100

    not using any video quality parameters but the default in the cloudinary ui

--- packages/utils/basics/setFileUrl.ts
+++ packages/utils/basics/setFileUrl.ts
@@ -12,15 +12,15 @@ export default (
     else if (dev) url = file.url as string;
     else url = file.secure_url as string;
 
-    const regex = /(?<=video\/upload\/)[v0-9]+(?=\/youmeet-official)/;
-    const match = url.match(regex);
-    if (match && video) {
-      url = url.replace(match[0], "q_50,vc_auto");
-      const split = url.split(".");
-      const lastEl = split[split.length - 1];
-      const index = url.lastIndexOf(lastEl);
-      url = url.slice(0, index) + "webm";
-    }
+    // const regex = /(?<=video\/upload\/)[v0-9]+(?=\/youmeet-official)/;
+    // const match = url.match(regex);
+    // if (match && video) {
+    //   url = url.replace(match[0], "q_auto,vc_auto");
+    //   const split = url.split(".");
+    //   const lastEl = split[split.length - 1];
+    //   const index = url.lastIndexOf(lastEl);
+    //   url = url.slice(0, index) + "webm";
+    // }
     return url;
   }
   return undefined;
