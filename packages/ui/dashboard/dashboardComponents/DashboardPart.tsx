966bf5288f90da4b03ac89a3c4114b6cdb6d1ed8
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-11-06 09:42:42 +0100

    little modifs

--- packages/ui/dashboard/dashboardComponents/DashboardPart.tsx
+++ packages/ui/dashboard/dashboardComponents/DashboardPart.tsx
@@ -1,10 +1,30 @@
 import { BetaUser } from "@youmeet/gql/generated";
-import VideoPartLoading from "../../VideoPartLoading";
 import dynamic from "next/dynamic";
+import { Skeleton } from "@mui/material";
 
 const DashboardPartComponent = dynamic(
   () => import("./DashboardPartComponent"),
-  { ssr: false, loading: () => <VideoPartLoading /> }
+  {
+    ssr: false,
+    loading: () => (
+      <div className="flex flex-col gap-[6px] h-[400px] w-full xs:w-screen sm:w-screen">
+        <div className="indent-4 xs:indent-0 sm:indent-0 md:indent-0 text-justify p-[12px] h-[86px] box-border shadow-custom">
+          <Skeleton width={"100%"} height={16} />
+          <Skeleton width={"100%"} height={16} />
+          <Skeleton width={"20%"} height={16} />
+        </div>
+
+        <div className="flex-center flex-col w-full h-full gap-[12px] md2:flex-col flex-wrap content-start p-[24px] box-border shadow-custom max-h-[550px]">
+          <div className="w-full flex-center my-[24px]">
+            <Skeleton width={180} height={18} />
+          </div>
+          <div className="w-full flex-center flex-1 h-full">
+            <Skeleton width={"100%"} height={"100%"} />
+          </div>
+        </div>
+      </div>
+    ),
+  }
 );
 
 export default function DashboardPart({ profil }: { profil: BetaUser }) {
