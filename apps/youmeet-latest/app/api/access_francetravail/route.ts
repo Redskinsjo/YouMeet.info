f5561b2b6d416c80ef08fd42afd425cb30f3ea18
Jonathan Carnos <jonathan.carnos@gmail.com>
2024-11-22 21:28:58 +0100

    added condition for uniqueheader

--- apps/youmeet-latest/app/api/access_francetravail/route.ts
+++ apps/youmeet-latest/app/api/access_francetravail/route.ts
@@ -1,4 +1,3 @@
-import { dev } from "@youmeet/functions/imports";
 import { BACKEND_ERRORS, BACKEND_MESSAGES } from "@youmeet/types/api/backend";
 import { ContextRequest } from "@youmeet/types/ContextRequest";
 import { BackendError } from "@youmeet/utils/basics/BackendErrorClass";
@@ -10,45 +9,54 @@ export async function GET(req: NextRequest): Promise<Response> {
   const searchParams = req.nextUrl.searchParams;
 
   try {
-    const noCors = await noCorsMiddleware({
-      request: req,
-    } as unknown as ContextRequest);
-    console.log(noCors, "noCors");
-    if (noCors) {
-      const uri =
-        "https://entreprise.francetravail.fr/connexion/oauth2/access_token";
-      const client_id = `${process.env.FRANCE_TRAVAIL_CLIENT_ID}`;
-      const client_secret = `${process.env.FRANCE_TRAVAIL_CLIENT_SECRET}`;
-      const params = new URLSearchParams({ realm: "/partenaire" });
-      const grant_type = "client_credentials";
-      const scope = decodeURIComponent(searchParams.get("scope") ?? "");
+    const uniqueHeader = req.headers.get("x-domain-youmeet") || "";
 
-      console.log(
-        client_id.slice(20, client_id.length + 1),
-        client_secret.slice(20, client_secret.length + 1),
-        scope
-      );
-      const body = `grant_type=${grant_type}&client_id=${client_id}&client_secret=${client_secret}&scope=${scope}`;
+    console.log(uniqueHeader, "uniqueHeader");
+    if (!uniqueHeader) {
+      const noCors = await noCorsMiddleware({
+        request: req,
+      } as unknown as ContextRequest);
+      console.log(noCors, "noCors");
+      if (noCors) {
+        const uri =
+          "https://entreprise.francetravail.fr/connexion/oauth2/access_token";
+        const client_id = `${process.env.FRANCE_TRAVAIL_CLIENT_ID}`;
+        const client_secret = `${process.env.FRANCE_TRAVAIL_CLIENT_SECRET}`;
+        const params = new URLSearchParams({ realm: "/partenaire" });
+        const grant_type = "client_credentials";
+        const scope = decodeURIComponent(searchParams.get("scope") ?? "");
 
-      const endpoint = uri + "?" + params.toString();
+        console.log(
+          client_id.slice(20, client_id.length + 1),
+          client_secret.slice(20, client_secret.length + 1),
+          scope
+        );
+        const body = `grant_type=${grant_type}&client_id=${client_id}&client_secret=${client_secret}&scope=${scope}`;
 
-      const response = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body,
-      });
+        const endpoint = uri + "?" + params.toString();
 
-      const data = await response.json();
+        const response = await fetch(endpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body,
+        });
 
-      return Response.json(data);
-    } else {
-      throw new BackendError(
-        BACKEND_ERRORS.NOT_AUTHORIZED,
-        BACKEND_MESSAGES.NOT_AUTHORIZED
-      );
+        const data = await response.json();
+
+        return Response.json(data);
+      } else {
+        throw new BackendError(
+          BACKEND_ERRORS.NOT_AUTHORIZED,
+          BACKEND_MESSAGES.NOT_AUTHORIZED
+        );
+      }
     }
+    throw new BackendError(
+      BACKEND_ERRORS.NOT_AUTHORIZED,
+      BACKEND_MESSAGES.NOT_AUTHORIZED
+    );
   } catch (err: any) {
     return Response.json(await handleActionError(err), {
       status: err.status,
