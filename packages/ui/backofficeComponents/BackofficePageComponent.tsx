dac29952d95cddc8cd176fba17da82484f908be2
Jonathan Carnos <jonathan.carnos@gmail.com>
2025-06-02 13:01:38 +0200

    removed osx-temperature-sensor package

--- packages/ui/backofficeComponents/BackofficePageComponent.tsx
+++ packages/ui/backofficeComponents/BackofficePageComponent.tsx
@@ -19,7 +19,6 @@ export default function BackofficePageComponent({
     <Layout newStyles={{ maxWidth: "1200px", padding: "0px", width: "100%" }}>
       <div className="flex flex-col gap-[24px] w-full border-[0.5px] border-solid border-grey500">
         <BackofficeLeadsComponent data={leads} users={users} />
-
         <Layout newStyles={{ minWidth: "900px" }}>
           <MoreFeaturesLeads />
         </Layout>
